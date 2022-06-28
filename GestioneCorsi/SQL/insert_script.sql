2fd510ab32c9c9a1b69ce5fca098830303880985
Betacom <Betacom@Betacom-PCQ8DY>
2022-06-28 12:53:00 +0200

    Creati i vari script SQL per crud e aggiunto ojdbc.jar

---
+++ GestioneCorsi/SQL/insert_script.sql
@@ -0,0 +1,10 @@
+insert into amministratore values(666, 'Betacom', 'Roma', 'Pass$01');
+
+insert into docente values(1, 'Massimo', 'Ghianda', 'Ho fatto un sacco di cose belle e interessanti');
+insert into docente values(2, 'Curzio', 'Pratillo', 'Trenitalia me deve ancora pagà');
+insert into docente values(3, 'Franco', 'Franchi', 'I fil sono belli, ma preferisco insegnare');
+
+
+insert into corso values(1, 1, 'Java', TO_DATE('17/12/2015', 'DD/MM/YYYY'), TO_DATE('20/12/2015', 'DD/MM/YYYY'), 1200, 'Potrebbe essere leggermente complesso ma soddisfacente', 'AX12');
+
+commit
