a283d10c6de04673692c7c5175d70b1a6a238e5b
mark5041 <mark.beloso5041@gmail.com>
2022-06-28 17:25:57 +0200

    DAOException, GenericDAO e Adapter

---
+++ GestioneCorsi/src/it/betacom/businesscomponent/AdapterDAO.java
@@ -0,0 +1,39 @@
+package it.betacom.businesscomponent;
+
+import java.sql.Connection;
+
+import it.betacom.architecture.dao.DAOException;
+import it.betacom.architecture.dao.GenericDAO;
+
+public abstract class AdapterDAO<T> implements GenericDAO<T>{
+	
+	@Override
+	public void create(Connection conn, T entity) throws DAOException {
+	}
+	
+	@Override
+	public void update(Connection conn, T Entity) throws DAOException {
+		
+	}
+	
+	@Override
+	public void delete(Connection conn, long id) throws DAOException {
+		
+	}
+	
+	public void delete(Connection conn, long id1, long id2) throws DAOException {
+		
+	}
+	
+	@Override
+	public T getById(Connection conn, long id) throws DAOException {
+		return null;
+	}
+	
+	@Override
+	public T[] getAll(Connection conn) throws DAOException {
+		return null;
+	}
+	
+}
+
